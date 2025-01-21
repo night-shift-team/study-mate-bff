57405586d6bfbcb565a5392ff4cdf99437dcb343
blockmonkey <blockmonkey@naver.com>
2025-01-21 12:36:54 +0900

    init: spring boot

---
+++ src/main/java/com/studyMate/studyMate/StudyMateApplication.java
@@ -0,0 +1,13 @@
+package com.studyMate.studyMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyMate/studyMate/StudyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyMate.studyMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
