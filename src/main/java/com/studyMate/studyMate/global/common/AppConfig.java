a4e0d6da055338542349ccd157403176004edd3a
blockmonkey <blockmonkey@naver.com>
2025-01-21 12:54:12 +0900

    init: 타임존 utc 설정

---
+++ src/main/java/com/studyMate/studyMate/global/common/AppConfig.java
@@ -0,0 +1,25 @@
+package com.studyMate.studyMate.global.common;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+    private final Environment environment;
+
+    @PostConstruct
+    public void started() {
+        // 타임존 설정 (UTC)
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        String[] activeProfiles = environment.getActiveProfiles();
+        log.info("서버시간확인 : {}", LocalDateTime.now());
+    }
+}
