23474fe815663ce67160e73780af0a05c8ed3ad0
blockmonkey <blockmonkey@naver.com>
2025-02-06 14:44:48 +0900

    docs: user api swagger 문서 추가

--- src/main/java/com/studyMate/studyMate/domain/user/controller/UserController.java
+++ src/main/java/com/studyMate/studyMate/domain/user/controller/UserController.java
@@ -4,6 +4,12 @@ import com.studyMate.studyMate.domain.user.dto.*;
 import com.studyMate.studyMate.domain.user.entity.User;
 import com.studyMate.studyMate.domain.user.service.UserService;
 import com.studyMate.studyMate.global.config.RoleAuth;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.NoArgsConstructor;
@@ -16,38 +22,63 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
+@Tag(name = "User API")
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     @GetMapping("/")
     @RoleAuth
+    @Operation(summary = "유저정보조회 (*)", description = "유저정보를 조회하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(schema = @Schema(implementation = GetUserDto.class))})
+    })
     public GetUserDto getUserInfo(HttpServletRequest request) {
         long userId = (Long) request.getAttribute("userId");
         return userService.getActiveUserById(userId);
     }
 
     @PostMapping("/sign-up/local")
+    @Operation(summary = "로컬 회원가입", description = "로컬 회원가입 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(schema = @Schema(implementation = SignUpResponseDto.class))})
+    })
     public SignUpResponseDto signUp(@RequestBody @Validated SignUpRequestDto signUpRequestBody){
         return userService.signUpLocal(signUpRequestBody);
     }
 
     @PostMapping("/sign-in/local")
+    @Operation(summary = "로컬 로그인", description = "로컬 로그인 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(schema = @Schema(implementation = SignInResponseDto.class))})
+    })
     public SignInResponseDto signIn (@RequestBody @Validated SignInRequestDto signInRequestBody) {
         return userService.signInLocal(signInRequestBody);
     }
 
     @PostMapping("/sign-in/google")
+    @Operation(summary = "구글 로그인", description = "구글 로그인 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(schema = @Schema(implementation = SignInResponseDto.class))})
+    })
     public SignInResponseDto googleSignIn (@RequestBody @Validated GoogleSignInRequestDto googleSignInRequestBody) {
         return userService.signInGoogle(googleSignInRequestBody.googleCode());
     }
 
     @GetMapping("/email/duplicate")
+    @Operation(summary = "이메일 중복체크", description = "이메일 중복체크 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(schema = @Schema(implementation = boolean.class))})
+    })
     public boolean duplicateCheckEmail(@RequestParam("email") String email){
         return userService.checkDuplicateEmail(email);
     }
 
     @GetMapping("/nickname/duplicate")
+    @Operation(summary = "닉네임 중복체크", description = "닉네임 중복체크 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success", content = {@Content(schema = @Schema(implementation = boolean.class))})
+    })
     public boolean duplicateCheckNickname(@RequestParam("nickname") String nickname){
         return userService.checkDuplicateNickname(nickname);
     }
