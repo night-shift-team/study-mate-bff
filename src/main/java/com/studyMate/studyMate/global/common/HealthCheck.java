70436b6ecc7d9a9b1d0c7d9e3465b97e35c9d63d
blockmonkey <blockmonkey@naver.com>
2025-03-05 16:03:04 +0900

    docs: swagger health check

--- src/main/java/com/studyMate/studyMate/global/common/HealthCheck.java
+++ src/main/java/com/studyMate/studyMate/global/common/HealthCheck.java
@@ -1,12 +1,16 @@
 package com.studyMate.studyMate.global.common;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Common")
 public class HealthCheck {
     @GetMapping("health")
+    @Operation(summary = "Health Check", description = "헬스체크 API")
     public String health(){
         return "ok";
     }
