687c6c6de24a74f70f7a8da5dd0901b0ac5617a0
blockmonkey <blockmonkey@naver.com>
2025-03-18 17:16:23 +0900

    feat: admin api 인가 권한 적용

--- src/main/java/com/studyMate/studyMate/domain/question/controller/QuestionAdminController.java
+++ src/main/java/com/studyMate/studyMate/domain/question/controller/QuestionAdminController.java
@@ -18,13 +18,14 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/question/admin")
-@Tag(name = "Question Admin API")
+@Tag(name = "Question Admin API (Required Role = 7+)")
 public class QuestionAdminController {
 
     private final QuestionService questionService;
 
     @GetMapping(value = "/{page}/{limit}/maq")
     @Operation(summary = "Admin Page MAQ Question 조회 (최신순)", description = "MAQ Question 전체 내역 조회")
+    @RoleAuth(requiredRole = 7)
     public MaqQuestionPageDto getMaqQuestionsLatest(
         @PathVariable("page") Integer page,
         @PathVariable("limit") Integer limit
@@ -34,6 +35,7 @@ public class QuestionAdminController {
 
     @GetMapping(value = "/{page}/{limit}/saq")
     @Operation(summary = "Admin Page SAQ Question 조회 (최신순)", description = "SAQ Question 전체 내역 조회")
+    @RoleAuth(requiredRole = 7)
     public SaqQuestionPageDto getSaqQuestionsLatest(
             @PathVariable("page") Integer page,
             @PathVariable("limit") Integer limit
@@ -44,6 +46,7 @@ public class QuestionAdminController {
 
     @PostMapping(value = "/maq")
     @Operation(summary = "Admin Page MAQ Question 생성", description = "MAQ Question 생성")
+    @RoleAuth(requiredRole = 7)
     public String getMaqQuestionsLatest(
             HttpServletRequest req,
             @RequestBody @Valid CreateMaqQuestionRequestDto requestDto
@@ -53,6 +56,7 @@ public class QuestionAdminController {
 
     @PostMapping(value = "/saq")
     @Operation(summary = "Admin Page SAQ Question 생성", description = "SAQ Question 생성")
+    @RoleAuth(requiredRole = 7)
     public String getMaqQuestionsLatest(
             HttpServletRequest req,
             @RequestBody @Valid CreateSaqQuestionRequestDto requestDto
@@ -62,6 +66,7 @@ public class QuestionAdminController {
 
     @PatchMapping(value = "/{question-id}/maq")
     @Operation(summary = "Admin Page MAQ Question 수정", description = "MAQ Question 수정")
+    @RoleAuth(requiredRole = 7)
     public String updateMaqQuestion(
             HttpServletRequest req,
             @PathVariable("question-id") String questionId,
@@ -72,6 +77,7 @@ public class QuestionAdminController {
 
     @PatchMapping(value = "/{question-id}/saq")
     @Operation(summary = "Admin Page SAQ Question 수정", description = "SAQ Question 수정")
+    @RoleAuth(requiredRole = 7)
     public String updateSaqQuestion(
             HttpServletRequest req,
             @PathVariable("question-id") String questionId,
@@ -82,7 +88,7 @@ public class QuestionAdminController {
 
     @PostMapping("/generator/maq")
     @Operation(summary = "MAQ Question 가라데이터 생성기 (테스트 전용)", description = "Question 가라데이터 생성 API")
-//    @RoleAuth(requiredRole = 7)
+    @RoleAuth(requiredRole = 7)
     public boolean generateFakeQuestions() {
         questionService.generateFakeQuestions();
         return true;
