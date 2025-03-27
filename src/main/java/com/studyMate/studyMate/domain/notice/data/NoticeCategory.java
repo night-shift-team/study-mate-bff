79bd021aaf1e69bf35442cf2753758b4d900518f
blockmonkey <blockmonkey@naver.com>
2025-03-27 15:22:12 +0900

    docs: Notice Category 주석

--- src/main/java/com/studyMate/studyMate/domain/notice/data/NoticeCategory.java
+++ src/main/java/com/studyMate/studyMate/domain/notice/data/NoticeCategory.java
@@ -1,8 +1,8 @@
 package com.studyMate.studyMate.domain.notice.data;
 
 public enum NoticeCategory {
-    GENERAL,
-    URGENT,
-    EVENT,
-    MAINTENANCE
+    GENERAL, // 일반공지
+    URGENT, // 긴급공지
+    EVENT, // 이벤트
+    MAINTENANCE // 점검
 }
