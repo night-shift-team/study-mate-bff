608f8dfc26bf3fc8169964d27631bc08dae758cc
blockmonkey <blockmonkey@naver.com>
2025-04-10 12:09:30 +0900

    feat: Question Category Enum 목록 반환 메소드

--- src/main/java/com/studyMate/studyMate/domain/question/data/QuestionCategory.java
+++ src/main/java/com/studyMate/studyMate/domain/question/data/QuestionCategory.java
@@ -1,5 +1,6 @@
 package com.studyMate.studyMate.domain.question.data;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,12 +19,17 @@ public enum QuestionCategory {
     NETWORK_SAQ(Type.SAQ),
     DB_SAQ(Type.SAQ),
     DESIGN_SAQ(Type.SAQ),
-    LEVEL_TEST(Type.NONE);
+    LEVEL_TEST(Type.NONE)
+    ;
 
-
+    /**
+     * 대 분류용 (객관식, 주관식)
+     */
     public enum Type {
         MAQ, SAQ, NONE
     }
+
+    private final Type type;
 
     QuestionCategory(Type type) {
         this.type = type;
@@ -33,5 +39,30 @@ public enum QuestionCategory {
         return type;
     }
 
-    private final Type type;
+    /**
+     * LEVEL Test를 제외한 카테고리 목록을 반환
+     */
+    public static List<QuestionCategory> getQuestionCategoryList() {
+        return Arrays.stream(QuestionCategory.values())
+                .filter(qc -> qc != QuestionCategory.LEVEL_TEST)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 카테고리 리스트를 클라이언트에 출력할 뷰 네임으로 변경.
+     */
+    public static String getQuestionCategoryViewName(QuestionCategory originalCategory) {
+        return switch(originalCategory) {
+            case ALGORITHUM_MAQ -> "알고리즘 (객관식)";
+            case ALGORITHUM_SAQ -> "알고리즘 (객관식)";
+            case OS_MAQ -> "운영체제 (객관식)";
+            case OS_SAQ -> "운영체제 (주관식)";
+            case DB_MAQ -> "데이터베이스 (객관식)";
+            case DB_SAQ -> "데이터베이스 (주관식)";
+            case DESIGN_MAQ -> "디자인패턴 (객관식)";
+            case DESIGN_SAQ -> "디자인패턴 (주관식)";
+            case LEVEL_TEST -> "레벨테스트";
+            default -> "미등록 카테고리";
+        };
+    }
 }
