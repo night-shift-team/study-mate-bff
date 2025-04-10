d513b72c15850cbfdce78e667d5262d894a57042
blockmonkey <blockmonkey@naver.com>
2025-04-10 12:10:38 +0900

    feat: Question Category dto

--- src/main/java/com/studyMate/studyMate/domain/question/dto/CategoryDetailDto.java
+++ src/main/java/com/studyMate/studyMate/domain/question/dto/CategoryDetailDto.java
@@ -3,15 +3,15 @@ package com.studyMate.studyMate.domain.question.dto;
 import com.studyMate.studyMate.domain.question.data.QuestionCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-@Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetQuestionCategoryInfoResponseDto {
-    private Integer totalCategoryCount;
-    private List<CategoryDetailDto> detail;
+public class CategoryDetailDto {
+    private QuestionCategory categoryOriginName;
+    private String categoryViewName;
+    private Integer categoryQuestionCount;
+    private Integer userSolvingCount;
+    private Integer solvingLimit;
 }
