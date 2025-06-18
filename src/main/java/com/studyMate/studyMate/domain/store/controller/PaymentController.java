9209bca9c31e25831c8cc7f78cf4b1d36028491c
blockmonkey <blockmonkey@naver.com>
2025-06-18 18:02:32 +0900

    fix: [store] -> payment 컨트롤러 네이밍 수정

--- src/main/java/com/studyMate/studyMate/domain/store/controller/PaymentController.java
+++ src/main/java/com/studyMate/studyMate/domain/store/controller/PaymentController.java
@@ -3,6 +3,8 @@ package com.studyMate.studyMate.domain.store.controller;
 import com.studyMate.studyMate.domain.store.dto.PayAppRequestDto;
 import com.studyMate.studyMate.domain.store.service.PayAppService;
 import com.studyMate.studyMate.global.config.RoleAuth;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
@@ -15,11 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "store")
 @RequestMapping("/store")
 @AllArgsConstructor
-public class StoreController {
+public class PaymentController {
 
     private final PayAppService payAppService;
 
     @PostMapping("/payment/request")
+    @Operation(summary = "PayApp 결제 URL 호출 API (로그인 필수)", description = "페이앱 결제 URL 호출 메소드")
     @RoleAuth
     public String createPayment(
             HttpServletRequest req,
@@ -30,6 +33,8 @@ public class StoreController {
     }
 
     @PostMapping("/payment/callback")
+    @Hidden
+    @Operation(summary = "비호출 함수 (PayApp 콜백용)", description = "비호출 함수 (PayApp 콜백용)")
     public String handlePayAppCallback(HttpServletRequest req) {
         return payAppService.handlePayAppCallback(req);
     }
