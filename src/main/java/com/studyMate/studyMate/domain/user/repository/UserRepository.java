4683ac32439fd1b0e372448a5ddf967d54b31285
Blockmonkey <blockmonkey@naver.com>
2025-09-09 18:26:30 +0900

    refactor: [user] 유저조회 시, fetch join 적용

--- src/main/java/com/studyMate/studyMate/domain/user/repository/UserRepository.java
+++ src/main/java/com/studyMate/studyMate/domain/user/repository/UserRepository.java
@@ -2,14 +2,18 @@ package com.studyMate.studyMate.domain.user.repository;
 
 import com.studyMate.studyMate.domain.user.data.UserStatus;
 import com.studyMate.studyMate.domain.user.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository <User, String>, UserRepositoryCustom {
+    @EntityGraph(attributePaths = {"userOAuths"})
     Optional<User> findByUserIdAndStatus(String id, UserStatus status);
     Optional<User> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
