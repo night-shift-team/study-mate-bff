96b54bec735aa3c106c5616e98d07576db1b65a8
Blockmonkey <blockmonkey@naver.com>
2025-09-27 13:28:53 +0900

    feat: 회원 모듈 사용 redis key 생성

--- src/main/java/com/studyMate/studyMate/global/redis/RedisKeyFactory.java
+++ src/main/java/com/studyMate/studyMate/global/redis/RedisKeyFactory.java
@@ -17,4 +17,32 @@ public class RedisKeyFactory {
     public static String viewedBoard(Long boardId, String userId) {
         return String.format("viewed:board:%d:user:%s", boardId, userId);
     }
+
+    /**
+     * 유저 로컬 회원가입 이메일 인증 Redis Key
+     */
+    public static String signUpLocalUser(String userId) {
+        return String.format("signup:%s", userId);
+    }
+
+    /**
+     * 로컬 회원가입 이메일 인증 Verified Redis Key
+     */
+    public static String singupVerifiedLocalUser(String userId) {
+        return String.format("singup-verified:%s", userId);
+    }
+
+    /**
+     * 유저 Refresh Token Redis Key
+     */
+    public static String refreshTokenUser(String userId) {
+        return String.format("rf-token:%s", userId);
+    }
+
+    /**
+     * 유저 비밀번호 찾기 인증 메일 Redis Key
+     */
+    public static String findPwdUser(String userId) {
+        return String.format("find-pwd:%s", userId);
+    }
 }
