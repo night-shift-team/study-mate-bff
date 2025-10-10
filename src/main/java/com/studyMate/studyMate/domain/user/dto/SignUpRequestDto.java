f74bcba400248da6855a7071a9a5f5fda0ef4066
Blockmonkey <blockmonkey@naver.com>
2025-10-10 15:03:20 +0900

    refactor: 유저 비밀번호 제한 추가

--- src/main/java/com/studyMate/studyMate/domain/user/dto/SignUpRequestDto.java
+++ src/main/java/com/studyMate/studyMate/domain/user/dto/SignUpRequestDto.java
@@ -1,6 +1,8 @@
 package com.studyMate.studyMate.domain.user.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +15,13 @@ import lombok.NoArgsConstructor;
 public class SignUpRequestDto {
     @Email(message = "invalid email")
     String loginId;
+
+    @NotBlank(message = "invalid password")
+    @Pattern(
+            regexp = "^(?=.*[A-Z!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{6,}$",
+            message = "비밀번호는 6자 이상이며 대문자 또는 특수문자를 포함해야 합니다"
+    )
     String loginPw;
+
     String nickname;
 }
